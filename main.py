4a650be163896f287f9a596a112550847b24a583
ankitsharma5911 <ankitsharma450306@gmail.com>
2024-12-27 17:26:43 +0530

    data ingestion complete

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 from networksecurity.exception import NetworkSecurityException
 from networksecurity.logger import logging
 import sys
+from networksecurity.components.data_ingestion import DataIngestion
+# from networksecurity.components.data_validation import DataValidation
+# from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from networksecurity.entity.config_entity import TrainingPipelineConfig
+
+# from networksecurity.components.model_trainer import ModelTrainer
+# from networksecurity.entity.config_entity import ModelTrainerConfig
+ 
+
 
 if __name__=="__main__":
     try:
-        logging.info("Enter the try block")
-        a=1/0
-        print("This will not be printed",a)
+        trainingpipelineconfig=TrainingPipelineConfig()
+        dataingestionconfig=DataIngestionConfig(trainingpipelineconfig)
+        data_ingestion=DataIngestion(dataingestionconfig)
+        logging.info("Initiate the data ingestion")
+        dataingestionartifact=data_ingestion.initiate_data_ingestion()
+        logging.info("Data Initiation Completed")
+        print(dataingestionartifact)
+        
+        
+        
+        
     except Exception as e:
            raise NetworkSecurityException(e,sys)
