694491f7c6cf32a8a6e832bb391beacbbee5d543
Ankit Sharma <84199832+ankitsharma5911@users.noreply.github.com>
2025-01-08 10:23:16 +0000

    add docker and git actions

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -30,7 +30,7 @@ import sys
 class TrainingPipeline:
     def __init__(self):
         self.training_pipeline_config=TrainingPipelineConfig()
-        
+        self.s3_sync = S3Sync()
         
     def start_data_ingestion(self):
         try:
